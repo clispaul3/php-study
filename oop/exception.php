989293ccd69c4f5fddd9bfa52520515b7bbf0905
张洋斌 <253626232@qq.com>
2019-01-13 21:56:05 +0800

    commit by bash

--- oop/exception.php
+++ oop/exception.php
@@ -5,6 +5,7 @@ try {
     throw new Exception('error',1);
     echo '3 <hr/>'; // 这段代码不会执行
 } catch(Exception $e){
-   echo 'error:' . $e->getMessage() . '<hr/>';
+   echo 'error:' . $e->getMessage() . '<hr/>'; // 异常信息
+   echo 'error:' . $e->getCode() . '<br/>'; // 异常代号
 }
 echo '5 <br/>';
